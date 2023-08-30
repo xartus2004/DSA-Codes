065426d791a6f4299036716df700fcaf535ee870
paranjaydubey <paranjaydubey19@gmail.com>
2023-08-30 19:27:45 +0530

    random

--- Graphs/bfs.cpp
+++ Graphs/bfs.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
 
+void BFS(vector <int> adj[],int v,int s)
+{
+    bool visited[v+1]={0,};
+    queue <int> q;
+    visited[s]=1;
+    q.push(s);
+    while(q.empty()==false)
+    {
+        int u=q.front();
+        q.pop();
+        cout<<u<<"\t";
+        for(int v:adj[u])
+        {
+            if(visited[u]==false)
+            {
+                visited[v]=true;
+                q.push(u);
+            }
+        }
+    }
+}
+
 int main(){
-    bool v11[10]={0,};
-    for(auto a:v11)
-        cout<<a<<""
+
 }
---
+++ GreedyAlgo/FractionalKnapsack.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Item{
+    int profit,weight;
+
+    Item(int profit,int weight)
+    {
+        this->profit=profit;
+        this->weight=weight;
+    }
+};
+
+static bool cmp(Item a,Item b)
+{
+    double r1=(double)a.profit/(double)a.weight;
+    double r2=(double)b.profit/(double)b.weight;
+    return r1>r2;
+}
+
+double fractionalKnapsack(int W,struct Item arr[],int N)
+{
+    sort(arr,arr+N,cmp);
+    double totalProfit=0.0;
+
+    for(int i=0 ; i<N ; i++)
+    {
+        if(W>arr[i].weight)
+        {
+            W-=arr[i].weight;
+            totalProfit+=arr[i].weight*arr[i].profit;
+        }
+        else
+        {
+            totalProfit+=arr[i].profit*((double)W/(double)arr[i].weight);
+            break;
+        }
+    }
+    return totalProfit;
+}
+
+int main()
+{
+    int W = 50;
+    Item arr[] = { { 60, 10 }, { 100, 20 }, { 120, 30 } };
+    int N = sizeof(arr) / sizeof(arr[0]);
+    cout << fractionalKnapsack(W, arr, N);
+    return 0;
+}
