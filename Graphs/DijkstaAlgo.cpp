0d4c9504ee81f1491744c14ceea06e5c52755165
Paranjay Dubey <paranjaydubey19@gmail.com>
2023-09-08 14:22:23 +0530

    Djikstra'a Algorithm Added

---
+++ Graphs/DijkstaAlgo.cpp
@@ -0,0 +1,38 @@
+//Its a Greedy Algorithm
+//
+
+#include <bits/stdc++.h> 
+using namespace std; 
+#define V 4
+
+vector<int> djikstraAlgo(int graph[V][V],int src) 
+{ 
+
+	vector<int> dist(V,INT_MAX);
+	dist[src]=0;
+	vector<bool> finalised(V,false);
+
+	for (int count = 0; count < V-1 ; count++) 
+	{ 
+		int u = -1; 
+		for(int i=0;i<V;i++)
+		    if(!finalised[i] && (u==-1||dist[i]<dist[u]))
+		        u=i;
+		finalised[u] = true; 
+		for (int v = 0; v < V; v++)
+			if (graph[u][v]!=0 && finalised[v] == false) 
+				dist[v] = min(dist[v],dist[u]+graph[u][v]); 
+	}
+    return dist;
+}
+
+int main() 
+{ 
+	int graph[V][V] = { { 0, 50, 100, 0}, 
+						{ 50, 0, 30, 200 }, 
+						{ 100, 30, 0, 20 }, 
+						{ 0, 200, 20, 0 },}; 
+	for(int x: djikstraAlgo(graph,0))
+	    cout<<x<<" ";
+	return 0; 
+} 
---
+++ Graphs/KahnsAlgo.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> TopologicalSort(vector <int> adj[],int V)
+{
+    int Indegree[V]={0};
+    
+}
+
+int main()
+{
+
+}
