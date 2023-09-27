1c4378bfc54ac783d01c719fdb6f7cea6d8df979
paranjaydubey <paranjaydubey19@gmail.com>
2023-09-27 21:24:19 +0530

    random

--- Graphs/Minimal Spanning Tree/KruskalsAlgo.cpp
+++ Graphs/Minimal Spanning Tree/KruskalsAlgo.cpp
@@ -21,11 +21,13 @@ public:
         return parent[node] = findUPar(parent[node]);
     }
 
-    void unionByRank(int u, int v) {
+    void unionByRank(int u, int v) 
+    {
         int ulp_u = findUPar(u);
         int ulp_v = findUPar(v);
         if (ulp_u == ulp_v) return;
-        if (rank[ulp_u] < rank[ulp_v]) {
+        if (rank[ulp_u] < rank[ulp_v]) 
+        {
             parent[ulp_u] = ulp_v;
         }
         else if (rank[ulp_v] < rank[ulp_u]) {
